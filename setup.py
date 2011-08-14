e67a203035fdb41afa22631764eb426a2d7c06fb
Guillaume Esquevin <guillaume.esquevin@platypus-creation.com>
2011-08-14 18:24:06 +0200

    Fixed templates by adding a super

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-rosetta-grappelli",
-    version = "1.0",
+    version = "1.0.1",
     description = """
     Compatibility templates for django rosetta when using django-grappelli
     """,
