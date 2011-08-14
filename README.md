3f8156399fd09d536a0a89b436d5661140df6a0c
Guillaume Esquevin <guillaume.esquevin@platypus-creation.com>
2011-08-14 02:24:27 +0200

    Django-rosetta-grappelli , from the work of Haineault.com

---
+++ README.md
@@ -0,0 +1,21 @@
+Django-Rosetta-Grappelli
+===============
+
+Compatibility template for rosetta when grappelli is installed
+
+All credits should go to **Haineault.com** for creating grappelli-fit
+
+Installation
+------------
+
+Add rosetta-grappelli **before** `rosetta` in your installed apps:
+
+    INSTALLED_APPS = (
+        'grappelli',
+        ...
+        'rosetta-grappelli',
+        'rosetta',
+        ...
+    )
+
+That's it
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-rosetta-grappelli",
+    version = "1.0",
+    description = """
+    Compatibility templates for django rosetta when using django-grappelli
+    """,
+    author = "Platypus Creation",
+    author_email = "contact@platypus-creation.com",
+    url = "https://github.com/platypus-creation/django-rosetta-grappelli",
+    packages = find_packages(),
+    include_package_data=True,
+)
